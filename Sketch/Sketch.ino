0dab00cad33c917304fa1999034cb3be771f7ba0
Lilia <lilia.manukyan@hotmail.com>
2018-06-17 13:17:41 +0400

    SOS morse code signaler

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,7 +4,7 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-int led = 13;
+int led = 10;
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -15,8 +15,36 @@ void setup()
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	digitalWrite(led, HIGH);
-	delay(1000);
-	digitalWrite(led, LOW);
-	delay(1000);
+	for (int x = 0; x < 3; x++)
+	{
+		digitalWrite(led, HIGH);
+		delay(150);
+		digitalWrite(led, LOW);
+		delay(100);
+	}
+
+	// delay to cause gap between letters
+	delay(100);
+
+	for (int x = 0; x < 3; x++)
+	{
+		digitalWrite(led, HIGH);
+		delay(400);
+		digitalWrite(led, LOW);
+		delay(100);
+	}
+
+	// delay to cause gap between letters
+	delay(100);
+
+	for (int x = 0; x < 3; x++)
+	{
+		digitalWrite(led, HIGH);
+		delay(150);
+		digitalWrite(led, LOW);
+		delay(100);
+	}
+
+	// wait 5s before reporting next SOS signal
+	delay(5000);
 }
