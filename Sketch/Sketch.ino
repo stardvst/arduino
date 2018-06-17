42d064fae1288647df2118012330fcd6cc5544f1
Lilia <lilia.manukyan@hotmail.com>
2018-06-17 12:35:05 +0400

    blinking led

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -1,17 +1,22 @@
 /*
- Name:		Sketch.ino
- Created:	17-Jun-18 11:18:21
- Author:	liliam
+Name:		Sketch.ino
+Created:	17-Jun-18 11:18:21
+Author:	liliam
 */
+
+int led = 13;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-
+	pinMode(led, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-
+	digitalWrite(led, HIGH);
+	delay(1000);
+	digitalWrite(led, LOW);
+	delay(1000);
 }
