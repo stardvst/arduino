73ffaaaf3580ecb788f91dbd9092434e2b95ec67
Lilia <lilia.manukyan@hotmail.com>
2018-06-17 12:32:21 +0400

    Uploading empty project

---
+++ Sketch/Sketch.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		Sketch.ino
+ Created:	17-Jun-18 11:18:21
+ Author:	liliam
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+
+}
