329fe8b09cb87bde8b67e35e365a224ff1fe65d1
Lilia <lilia.manukyan@hotmail.com>
2018-06-23 18:51:42 +0400

    mood lamp

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,25 +4,59 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-int led = 11;
-float sinVal;
-int ledVal;
+float RGB1[3];
+float RGB2[3];
+float INC[3];
+
+int red, green, blue;
+
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	pinMode(led, OUTPUT);
+	Serial.begin(9600);
+	randomSeed(analogRead(0));
+
+	RGB1[0] = 0;
+	RGB1[1] = 0;
+	RGB1[2] = 0;
+
+	RGB1[0] = random(256);
+	RGB1[1] = random(256);
+	RGB1[2] = random(256);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	for (auto x = 0; x < 180; ++x)
+	randomSeed(analogRead(0));
+
+	for (auto x = 0; x < 3; ++x)
+		INC[x] = (RGB1[x] - RGB2[x]) / 256;
+
+	for (auto x = 0; x < 256; ++x)
 	{
-		// convert degrees to radians then obtain sin value
-		sinVal = sin(x * (3.1412 / 180));
-		ledVal = int(sinVal * 255);
-		analogWrite(led, ledVal);
-		delay(25);
+		red =	int(RGB1[0]);
+		green = int(RGB1[1]);
+		blue =	int(RGB1[2]);
+
+		analogWrite(redPin, red);
+		analogWrite(greenPin, green);
+		analogWrite(bluePin, blue);
+		delay(100);
+
+		RGB1[0] -= INC[0];
+		RGB1[1] -= INC[1];
+		RGB1[2] -= INC[2];
+	}
+
+	for (float & x : RGB2)
+	{
+		x = random(556) - 300;
+		x = constrain(x, 0, 255);
+		delay(100);
 	}
 }
