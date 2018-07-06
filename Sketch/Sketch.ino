c3c61d959bfcbbe9d059ffc8c6bf956bca0691d7
Lilia <lilia.manukyan@hotmail.com>
2018-07-07 00:02:07 +0400

    led + beep (piezo speaker)

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,25 +4,24 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-int led = 6;
-int ldr = 0;
-int lightVal = 0;
+int led = 4;
+int beep = 5;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
 	pinMode(led, OUTPUT);
+	pinMode(beep, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	lightVal = analogRead(ldr);
 	digitalWrite(led, HIGH);
-
-	delay(lightVal);
+	digitalWrite(beep, HIGH);
+	delay(250);
 
 	digitalWrite(led, LOW);
-
-	delay(lightVal);
+	digitalWrite(beep, LOW);
+	delay(250);
 }
