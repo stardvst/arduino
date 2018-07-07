16416983686a3984698abb41745baadaf056a378
Lilia <lilia.manukyan@hotmail.com>
2018-07-07 12:35:56 +0400

    lights out + thief arrived (lol)

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -6,24 +6,25 @@ Author:	liliam
 
 int input = A0;
 int LED = 10;
+int pir = 7;
+
 int sensorValue = 0;
+int moved = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	Serial.begin(9600);
 	pinMode(LED, OUTPUT);
+	pinMode(pir, INPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
 	sensorValue = analogRead(input);
+	moved = digitalRead(pir);
 
-	Serial.print("Sensor value = ");
-	Serial.print(sensorValue);
-
-	digitalWrite(LED, sensorValue < 50 ? HIGH : LOW);
+	digitalWrite(LED, sensorValue < 50 && moved == HIGH ? HIGH : LOW);
 
 	delay(50);
 }
