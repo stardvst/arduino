f4def3e585bfed8524ceba4e0b2bc61b94f4e3aa
Lilia <lilia.manukyan@hotmail.com>
2018-07-07 12:13:02 +0400

    motion detector!

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,24 +4,18 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-int led = 4;
-int beep = 5;
+int led = 5;
+int pir = 7;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
 	pinMode(led, OUTPUT);
-	pinMode(beep, OUTPUT);
+	pinMode(pir, INPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	digitalWrite(led, HIGH);
-	digitalWrite(beep, HIGH);
-	delay(250);
-
-	digitalWrite(led, LOW);
-	digitalWrite(beep, LOW);
-	delay(250);
+	digitalWrite(led, digitalRead(pir));
 }
