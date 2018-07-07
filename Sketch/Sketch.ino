7c9bfb46d0e2566211026bed24553b6c413ad2e9
Lilia <lilia.manukyan@hotmail.com>
2018-07-07 18:37:29 +0400

    servo

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,36 +4,30 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-#include <IRRemote/IRremote.h>
+#include <Servo.h>
 
-int RECV_PIN = 11;
+Servo servo;
 
-IRrecv irrecv(RECV_PIN);
-
-decode_results results;
+int degree = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	Serial.begin(9600);
-
-	pinMode(13, OUTPUT);
-
-	irrecv.enableIRIn();
+	servo.attach(8); // pin 8
 }
-olling 
+
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	if (irrecv.decode(&results))
+	for (auto angle = 0; angle < 180; ++angle)
 	{
-		Serial.println(results.value, DEC);
+		servo.write(angle);
+		delay(10);
+	}
 
-		if (results.value == 16724175)
-			digitalWrite(13, HIGH);
-		else if (results.value == 16718055)
-			digitalWrite(13, LOW);
-
-		irrecv.resume();
+	for (auto angle = 180; angle >= 0; --angle)
+	{
+		servo.write(angle);
+		delay(10);
 	}
 }
