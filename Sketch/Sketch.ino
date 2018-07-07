84d764881471a6d05c47b3b96cb2e39a49d786c1
Lilia <lilia.manukyan@hotmail.com>
2018-07-07 15:07:26 +0400

    distance measurement

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,27 +4,42 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-int input = A0;
-int LED = 10;
-int pir = 7;
+int trigger =  7;
+int echo = 6;
 
-int sensorValue = 0;
-int moved = 0;
+int time = 0;
+int dist = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	pinMode(LED, OUTPUT);
-	pinMode(pir, INPUT);
+	Serial.begin(9600);
+
+	pinMode(echo, INPUT);
+	pinMode(trigger, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	sensorValue = analogRead(input);
-	moved = digitalRead(pir);
+	digitalWrite(trigger, LOW);
+	delay(5);
 
-	digitalWrite(LED, sensorValue < 50 && moved == HIGH ? HIGH : LOW);
+	digitalWrite(trigger, HIGH);
+	delay(10);
 
-	delay(50);
+	digitalWrite(trigger, LOW);
+
+	time = pulseIn(echo, HIGH);
+	dist = (time / 2) / 29.1;
+
+	if (dist >= 500 || dist <= 0)
+		Serial.println("No measurement.");
+	else
+	{
+		Serial.print(dist);
+		Serial.println("cm");
+	}
+
+	delay(1000);
 }
