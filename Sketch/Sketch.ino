068fb62a5ee92541403f4fc7ec807be3206cac26
Lilia <lilia.manukyan@hotmail.com>
2018-07-24 14:30:17 +0400

    relay shield

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,30 +4,17 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-#include <Servo.h>
-
-Servo servo;
-
-int degree = 0;
-
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	servo.attach(8); // pin 8
+	pinMode(6, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	for (auto angle = 0; angle < 180; ++angle)
-	{
-		servo.write(angle);
-		delay(10);
-	}
-
-	for (auto angle = 180; angle >= 0; --angle)
-	{
-		servo.write(angle);
-		delay(10);
-	}
+	digitalWrite(6, HIGH);
+	delay(500);
+	digitalWrite(6, LOW);
+	delay(500);
 }
