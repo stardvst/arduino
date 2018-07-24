a79f4ffe725400a2daf833da86b0fd95af8a104d
Lilia <lilia.manukyan@hotmail.com>
2018-07-24 14:55:16 +0400

    stepper motor

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,17 +4,22 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
+#include <Stepper.h>
+
+int SPMU = 32;
+Stepper stepper{ SPMU, 2,3,4,5 };
+
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	pinMode(6, OUTPUT);
+	stepper.setSpeed(500);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	digitalWrite(6, HIGH);
+	stepper.step(2048);
 	delay(500);
-	digitalWrite(6, LOW);
+	stepper.step(-2048);
 	delay(500);
 }
