70ea7bbf0dc728421e2c8d4596de32c75287b2f3
Lilia <lilia.manukyan@hotmail.com>
2019-01-02 18:29:11 +0400

    Water drop detection

--- Sketch/Sketch.ino
+++ Sketch/Sketch.ino
@@ -4,22 +4,26 @@ Created:	17-Jun-18 11:18:21
 Author:	liliam
 */
 
-#include <Stepper.h>
-
-int SPMU = 32;
-Stepper stepper{ SPMU, 2,3,4,5 };
+int measurement = 0;
+int beep = 6;
 
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-	stepper.setSpeed(500);
+	Serial.begin(9600);
+	pinMode(6, OUTPUT);
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop()
 {
-	stepper.step(2048);
+	measurement = analogRead(A0);
+	Serial.print("Moisture measurement: ");
+	Serial.print(measurement);
 	delay(500);
-	stepper.step(-2048);
-	delay(500);
+	if (measurement > 100)
+		digitalWrite(beep, HIGH);
+	else
+		digitalWrite(beep, LOW);
 }
